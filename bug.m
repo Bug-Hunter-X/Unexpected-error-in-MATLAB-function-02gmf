8300a9a3247a755333d34e2d4015d59198b17ca2
BugHunter-X <askzenai@gmail.com>
2024-12-09 02:40:05 +0100

    Add bug.m file

---
+++ bug.m
@@ -0,0 +1,9 @@
+function result = myFunction(input)
+  % Some code here that might cause an error
+  ...
+  result = someValue;
+end
+
+% Example usage
+input = [1,2,3];
+result = myFunction(input);
